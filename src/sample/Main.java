b2f12a02098d88bb78b1d1ea62a8c65d530ebe9d
fabian <fmorera2015@gmail.com>
2021-06-14 23:38:16 -0600

    Calculator Second commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,25 +1,67 @@
 package sample;
 
+import sample.calculator.Calculator;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int first,second, add, substract, multiply, divide;
-        System.out.println("Escriba dos numeros");
-        first = entrada.nextInt();
-        second = entrada.nextInt();
+        Calculator calcu = new Calculator();
 
-        substract = first-second;
-        multiply = first*second;
-        divide = first/second;
-        add = first+second;
+        Scanner entry = new Scanner(System.in);
 
-        System.out.println(add);
-        System.out.println(substract);
-        System.out.println(multiply);
-        System.out.println(divide);
-        
+        boolean exit = false;
+        //MENU
+        while(!exit){
+        System.out.println("1.sumar");
+        System.out.println("2.restar");
+        System.out.println("3.multiplicar");
+        System.out.println("4.dividir");
+        System.out.println("5.Salir");
+
+        System.out.println("seleccione un numero");
+        int option = entry.nextInt();
+
+        switch (option){
+            //ADD
+            case 1:
+
+        System.out.println("Ingrese los 2 números a sumar");
+        int result = calcu.add(entry.nextInt(), entry.nextInt());
+        System.out.println("El resultado de la suma "+result);
+
+                break;
+            //SUBSTRACT
+            case 2:
+
+        System.out.println("Ingrese los 2 números a restar");
+        result = calcu.substract(entry.nextInt(), entry.nextInt());
+        System.out.println("El resultado de la resta "+result);
+
+                break;
+            //MULTIPLY
+            case 3:
+        System.out.println("Ingrese los 2 números a multiplicar");
+        result = calcu.multiply(entry.nextInt(), entry.nextInt());
+        System.out.println("El resultado de la multiplicacion es "+result);
+
+                break;
+            //DIVIDE
+            case 4:
+        System.out.println("Ingrese los 2 números a dividir");
+        result = calcu.divide(entry.nextInt(), entry.nextInt());
+        System.out.println("El resultado de la división "+result);
+
+                break;
+            //EXI
+            case 5:
+            exit = true;
+                System.out.println("Muchas Gracias");
+                break;
+            default:
+                System.out.println("INVALIDO");
+            }
+        }
     }
 }
--- src/sample/calculator/Calculator.java
+++ src/sample/calculator/Calculator.java
@@ -1,10 +1,40 @@
 package sample.calculator;
 
-import java.util.Scanner;
+
 
 public class Calculator {
-    Scanner entrada = new Scanner(System.in);
-    int entero;
+    //ADD
+    public int add(int a, int b){
+        int result=0;
+        if(a>0 && b>0){
+            result = a+b;
+        }
+        return result;
+    }
+    //SUBSTRACT
+    public int substract(int a, int b){
+        int result=0;
+        if(a>0 && b>0){
+            result = a-b;
+        }
+        return result;
+    }
+    //MULTIPLY
+    public int multiply(int a, int b){
+        int result=0;
+        if(a>0 && a>0){
+            result = a*b;
+        }
+        return result;
+    }
+    //DIVIDE
+    public int divide(int a, int b){
+        int result=0;
+        if (a>0 && b>0){
+            result=a/b;
+        }
+        return result;
+    }
 
 }
 
